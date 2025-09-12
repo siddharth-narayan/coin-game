023a5e91f7e5a1a3edfc82c91e26e25611c39cd2
Siddharth <siddharth.060605@gmail.com>
2025-09-12 15:01:34 -0500

    Add Client STUN request announcement

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,31 @@
+use std::net::UdpSocket;
+use std::net::{SocketAddr, ToSocketAddrs};
+use stunclient::StunClient;
+
 fn main() {
-    println!("Hello, world!");
+    let local_addr: SocketAddr = "0.0.0.0:0".parse().unwrap();
+    let stun_addr = "stun.l.google.com:19302"
+        .to_socket_addrs()
+        .unwrap()
+        .filter(|x| x.is_ipv4())
+        .next()
+        .unwrap();
+    let udp = UdpSocket::bind(local_addr).unwrap();
+
+    let c = StunClient::new(stun_addr);
+
+    let my_external_addr: SocketAddr = c.query_external_address(&udp).unwrap();
+
+    println!("my_external_addr {}", my_external_addr);
+
+    let my_external_addr: SocketAddr = c.query_external_address(&udp).unwrap();
+
+    println!("my_external_addr {}", my_external_addr);
+
+    let http_client = reqwest::blocking::Client::new();
+
+    let body = format!("{{\"ip\":\"{}\", \"port\":{} }}", my_external_addr.ip(), my_external_addr.port());
+    let res = http_client.post("http://localhost:3000/announce")
+    .body(body.clone()).send();
+    println!("{}", body);
 }
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,23 @@
 import { Hono } from 'hono'
-import { serve, type HttpBindings } from '@hono/node-server'
+import { serve } from '@hono/node-server'
+import { z } from "zod"
+const app = new Hono()
 
-const app = new Hono<{Bindings: HttpBindings}>
-
-app.all('/announce', (c) => {
-    let ip = c.env.incoming.socket.remoteAddress
-    console.log(ip)
-    return c.text("", 200)
+let announceSchema = z.object({
+    ip: z.ipv4().or(z.ipv6()),
+    port: z.int().positive().lt(65536)
 })
+
+let peers: z.infer<typeof announceSchema>[] = []
+
+app.post('/announce', async (c) => {
+    let json = await c.req.json()
+    
+    let res = z.parse(announceSchema, json)
+    console.log(res)
+
+    peers.push(res)
+    return c.json({peers: peers}, 200)
+})
+
 serve(app)
