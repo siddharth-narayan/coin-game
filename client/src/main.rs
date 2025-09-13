5b3f61b8d53e52b30fc67ff22c5df5242b659203
Siddharth <siddharth.060605@gmail.com>
2025-09-12 19:58:15 -0500

    simplify socket and addr get call

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,31 +1,19 @@
 use std::net::UdpSocket;
 use std::net::{SocketAddr, ToSocketAddrs};
-use stunclient::StunClient;
+use stunclient::{just_give_me_the_udp_socket_and_its_external_address as socket_and_addr};
 
 fn main() {
-    let local_addr: SocketAddr = "0.0.0.0:0".parse().unwrap();
-    let stun_addr = "stun.l.google.com:19302"
-        .to_socket_addrs()
-        .unwrap()
-        .filter(|x| x.is_ipv4())
-        .next()
-        .unwrap();
-    let udp = UdpSocket::bind(local_addr).unwrap();
+    let (udp, addr): (UdpSocket, SocketAddr) = socket_and_addr();
 
-    let c = StunClient::new(stun_addr);
-
-    let my_external_addr: SocketAddr = c.query_external_address(&udp).unwrap();
-
-    println!("my_external_addr {}", my_external_addr);
-
-    let my_external_addr: SocketAddr = c.query_external_address(&udp).unwrap();
-
-    println!("my_external_addr {}", my_external_addr);
+    println!("my_external_addr {}", addr);
 
     let http_client = reqwest::blocking::Client::new();
 
-    let body = format!("{{\"ip\":\"{}\", \"port\":{} }}", my_external_addr.ip(), my_external_addr.port());
+    let body = format!("{{\"ip\":\"{}\", \"port\":{} }}", addr.ip(), addr.port());
     let res = http_client.post("http://localhost:3000/announce")
     .body(body.clone()).send();
     println!("{}", body);
+
+    udp.
+
 }
